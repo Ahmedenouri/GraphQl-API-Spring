751d9c4b5c21f206e99f6ae44f49d2559466b869
ahmed <ahmedenouri7@gmail.com>
2024-06-16 22:43:01 +0100

    initial project GraphQl/Service with JPA Spring Boot.

---
+++ src/test/java/ma/formation/webService/graphQl/GraphQlApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.formation.webService.graphQl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphQlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
